27c8b5d4d03a862d676980c21819bcae872b2e9f
Hamza BENHMANI <hamza@plot.ly>
2021-09-01 18:30:15 -0400

    First my-synth-loot commit

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,71 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { Contract } from "@ethersproject/contracts";
 import { getDefaultProvider } from "@ethersproject/providers";
 import { useQuery } from "@apollo/react-hooks";
 
-import { Body, Button, Header, Image, Link } from "./components";
-import logo from "./ethereumLogo.png";
+import { Body, Button, Header } from "./components";
 import useWeb3Modal from "./hooks/useWeb3Modal";
 
 import { addresses, abis } from "@project/contracts";
 import GET_TRANSFERS from "./graphql/subgraph";
 
-async function readOnChainData() {
-  // Should replace with the end-user wallet, e.g. Metamask
+async function getSynthLoot(address) {
   const defaultProvider = getDefaultProvider();
-  // Create an instance of an ethers.js Contract
-  // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
-  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
-  // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
-  console.log({ tokenBalance: tokenBalance.toString() });
+  const lootContract = new Contract(addresses.syntheticLoot, abis.syntheticLootAbi, defaultProvider);
+  const tokenURI = await lootContract.tokenURI(address);
+  const lootMetadata = JSON.parse(atob(tokenURI.replace('data:application/json;base64,','')));
+  const lootImage = lootMetadata["image"];
+  return (lootImage)
 }
 
 function WalletButton({ provider, loadWeb3Modal, logoutOfWeb3Modal }) {
   return (
-    <Button
-      onClick={() => {
-        if (!provider) {
-          loadWeb3Modal();
-        } else {
-          logoutOfWeb3Modal();
-        }
-      }}
-    >
-      {!provider ? "Connect Wallet" : "Disconnect Wallet"}
-    </Button>
+    <div>
+      {provider ? <p>{provider.provider.selectedAddress}</p> : <div></div>}
+      <Button
+        onClick={() => {
+          if (!provider) {
+            loadWeb3Modal();
+          } else {
+            logoutOfWeb3Modal();
+          }
+        }}
+      >
+        {!provider ? "Connect Wallet" : "Disconnect Wallet"}
+      </Button>
+    </div>
   );
 }
 
 function App() {
   const { loading, error, data } = useQuery(GET_TRANSFERS);
   const [provider, loadWeb3Modal, logoutOfWeb3Modal] = useWeb3Modal();
+  const [synthLootImg, setSynthLootImg] = useState("");
 
   React.useEffect(() => {
-    if (!loading && !error && data && data.transfers) {
-      console.log({ transfers: data.transfers });
+    if (loading) {
+      setSynthLootImg("https://mir-s3-cdn-cf.behance.net/project_modules/fs/b6e0b072897469.5bf6e79950d23.gif")
     }
+    if (!loading && !error && data && data.transfers) {}
   }, [loading, error, data]);
+
+  if (provider) {
+    getSynthLoot(provider.provider.selectedAddress).then((image) => {
+      setSynthLootImg(image);
+    });
+  }
 
   return (
     <div>
       <Header>
-        <WalletButton provider={provider} loadWeb3Modal={loadWeb3Modal} logoutOfWeb3Modal={logoutOfWeb3Modal} />
+        <WalletButton provider={provider} loadWeb3Modal={loadWeb3Modal} logoutOfWeb3Modal={logoutOfWeb3Modal}  />
       </Header>
       <Body>
-        <Image src={logo} alt="react-logo" />
-        <p>
-          Edit <code>packages/react-app/src/App.js</code> and save to reload.
-        </p>
-        {/* Remove the "hidden" prop and open the JavaScript console in the browser to see what this function does */}
-        <Button hidden onClick={() => readOnChainData()}>
-          Read On-Chain Balance
-        </Button>
-        <Link href="https://ethereum.org/developers/#getting-started" style={{ marginTop: "8px" }}>
-          Learn Ethereum
-        </Link>
-        <Link href="https://reactjs.org">Learn React</Link>
-        <Link href="https://thegraph.com/docs/quick-start">Learn The Graph</Link>
+        {provider ? <img alt="Synthetic Loot of your wallet" src={synthLootImg}></img> : <div></div>}
       </Body>
     </div>
   );
